6ad581d182b4df16d3272f94e127449e44b59cee
Lukasz Pakula <lukasz.pakula@atheon.co.uk>
2021-05-25 10:55:47 +0100

    Fix urls

--- worldpay/urls.py
+++ worldpay/urls.py
@@ -1,22 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, re_path
 from django.views.decorators.csrf import csrf_exempt
 
 from worldpay import views
 
 
-urlpatterns = ['',
-    # Override for basket preview that adds the worldpay form
-    url(r'preview/$',
-        views.PaymentDetailsView.as_view(preview=True),
-        name='worldpay-preview'),
-    
-    # Callback
-    url(r'callback$', csrf_exempt(views.CallbackResponseView.as_view()),
-        name='worldpay-callback'),
-
-    url(r'success$', views.SuccessView.as_view(),
-        name='worldpay-success'),
-    url(r'fail$', views.FailView.as_view(),
-        name='worldpay-fail'),
-    
+urlpatterns = [
+    re_path(r'preview/$', views.PaymentDetailsView.as_view(preview=True), name='worldpay-preview'),
+    re_path(r'callback$', csrf_exempt(views.CallbackResponseView.as_view()), name='worldpay-callback'),
+    re_path(r'success$', views.SuccessView.as_view(), name='worldpay-success'),
+    re_path(r'fail$', views.FailView.as_view(), name='worldpay-fail'),
 ]
